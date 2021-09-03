35a658e7ce4a33f52adc6f087fad554ebd574d78
zdurr22 <zdurr22@gmail.com>
2021-09-03 16:21:48 -0400

    commit tree 8c52bdc4bfbbaacce68715c229f1e47b977f95c8

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create, :home]
+
+    def home
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        if @user = User.find_by(name: params[:user][:name])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render 'new'
+        end
+    end
+    
+    def destroy
+        session.delete("user_id")
+        redirect_to root_path
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  resources :attractions
+
+  root 'session#home'
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy'
+  post '/rides', to: 'ride#create', as: 'rides'
+
 end
